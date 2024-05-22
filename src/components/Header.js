d2f800865d3c2baf825d297466eaf0cc32b47a57
Ahmed Mohamed <ahmedmohamed.amin@hotmail.com>
2024-05-22 20:46:21 +0300

    Add media query for mobile responsiveness

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,11 @@
 function Header() {
   return (
     <header className="app-header">
-      <img src="Lotr-ring.png" alt="lord of the rings ring" />
+      <img
+        className="ring-image"
+        src="Lotr-ring.png"
+        alt="lord of the rings ring"
+      />
       <h1>The Lotr Quiz</h1>
     </header>
   );
